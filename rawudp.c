b5e304fb43218191fe20cbd2f9ba338296c968cb
aymeric baurain <aymeric baurain>
2017-10-21 18:55:01 +0200

    read file

--- rawudp.c
+++ rawudp.c
@@ -8,6 +8,8 @@
 #include<errno.h> //For errno - the error number
 #include<netinet/udp.h>   //Provides declarations for udp header
 #include<netinet/ip.h>    //Provides declarations for ip header
+#include <arpa/inet.h>
+#define TAILLE_MAX 512
 
 /*
     96 bit (12 bytes) pseudo header needed for udp header checksum calculation
@@ -24,6 +26,24 @@ struct pseudo_header
 /*
     Generic checksum calculation function
 */
+private char readFileToSend(char nameFile){
+    FILE* fichier = NULL;
+    char chaine[TAILLE_MAX] = "";
+
+    fichier = fopen(nameFile, "r");
+
+    if (fichier != NULL)
+    {
+        while (fgets(chaine, TAILLE_MAX, fichier) != NULL) // On lit le fichier tant qu'on ne reçoit pas d'erreur (NULL)
+        {
+            printf("%s", chaine); // On affiche la chaîne qu'on vient de lire
+        }
+
+        fclose(fichier);
+    }
+
+}
+
 unsigned short csum(unsigned short *ptr,int nbytes)
 {
     register long sum;
@@ -48,9 +68,19 @@ unsigned short csum(unsigned short *ptr,int nbytes)
     return(answer);
 }
 
-int main (void)
+int main(int argc, char *argv[])
 {
-    //Create a raw socket of type IPPROTO
+    printf("taille %d \n", argc);
+    printf("arg1 %s \n",argv[1]);
+    printf("arg2 %s \n",argv[2]);
+
+    char packet[4096] , source_ip[32] , *payload , *pseudogram;
+
+    if (argc != 2) {
+        fprintf(stderr,"usage: talker hostname message\n");
+        exit(1);
+    }
+
     int s = socket (AF_INET, SOCK_RAW, IPPROTO_RAW);
 
     if(s == -1)
@@ -60,37 +90,36 @@ int main (void)
         exit(1);
     }
 
-    //Datagram to represent the packet
-    char datagram[4096] , source_ip[32] , *data , *pseudogram;
 
     //zero out the packet buffer
-    memset (datagram, 0, 4096);
+    memset (packet, 0, 4096);
 
     //IP header
-    struct iphdr *iph = (struct iphdr *) datagram;
+    struct iphdr *iph = (struct iphdr *) packet;
 
     //UDP header
-    struct udphdr *udph = (struct udphdr *) (datagram + sizeof (struct ip));
+    struct udphdr *udph = (struct udphdr *) (packet + sizeof (struct ip));
 
     struct sockaddr_in sin;
     struct pseudo_header psh;
 
-    //Data part
-    data = datagram + sizeof(struct iphdr) + sizeof(struct udphdr);
-    strcpy(data , "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    //Data part pointe a la fin du packet udph
+    payload = packet + sizeof(struct iphdr) + sizeof(struct udphdr);
+    readFileToSend(argv[2]);
+    strcpy(payload , "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 
     //some address resolution
     strcpy(source_ip , "192.168.159.138");
 
     sin.sin_family = AF_INET;
     sin.sin_port = htons(80);
-    sin.sin_addr.s_addr = inet_addr ("192.168.159.136");
+    sin.sin_addr.s_addr = inet_addr("192.168.159.136");
 
     //Fill in the IP Header
     iph->ihl = 5;
     iph->version = 4;
     iph->tos = 0;
-    iph->tot_len = sizeof (struct iphdr) + sizeof (struct udphdr) + strlen(data);
+    iph->tot_len = sizeof (struct iphdr) + sizeof (struct udphdr) + strlen(payload);
     iph->id = htonl (54321); //Id of this packet
     iph->frag_off = 0;
     iph->ttl = 255;
@@ -100,12 +129,12 @@ int main (void)
     iph->daddr = sin.sin_addr.s_addr;
 
     //Ip checksum
-    iph->check = csum ((unsigned short *) datagram, iph->tot_len);
+    iph->check = csum ((unsigned short *) packet, iph->tot_len);
 
     //UDP header
     udph->source = htons (5050);
     udph->dest = htons (5050);
-    udph->len = htons(8 + strlen(data)); //tcp header size
+    udph->len = htons(8 + strlen(payload)); //tcp header size
     udph->check = 0; //leave checksum 0 now, filled later by pseudo header
 
     //Now the UDP checksum using the pseudo header
@@ -113,21 +142,20 @@ int main (void)
     psh.dest_address = sin.sin_addr.s_addr;
     psh.placeholder = 0;
     psh.protocol = IPPROTO_UDP;
-    psh.udp_length = htons(sizeof(struct udphdr) + strlen(data) );
+    psh.udp_length = htons(sizeof(struct udphdr) + strlen(payload) );
 
-    int psize = sizeof(struct pseudo_header) + sizeof(struct udphdr) + strlen(data);
+    int psize = sizeof(struct pseudo_header) + sizeof(struct udphdr) + strlen(payload);
     pseudogram = malloc(psize);
 
     memcpy(pseudogram , (char*) &psh , sizeof (struct pseudo_header));
-    memcpy(pseudogram + sizeof(struct pseudo_header) , udph , sizeof(struct udphdr) + strlen(data));
+    memcpy(pseudogram + sizeof(struct pseudo_header) , udph , sizeof(struct udphdr) + strlen(payload));
 
     udph->check = csum( (unsigned short*) pseudogram , psize);
 
-    //loop if you want to flood :)
     //while (1)
     {
         //Send the packet
-        if (sendto (s, datagram, iph->tot_len ,  0, (struct sockaddr *) &sin, sizeof (sin)) < 0)
+        if (sendto (s, packet, iph->tot_len ,  0, (struct sockaddr *) &sin, sizeof (sin)) < 0)
         {
             perror("sendto failed");
         }
@@ -141,4 +169,3 @@ int main (void)
     return 0;
 }
 
-//Complete
