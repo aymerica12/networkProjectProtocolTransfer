91e0b29c10479b4a1573b0b160f6f4582fb6e966
aymeric baurain <aymeric baurain>
2017-10-30 09:56:23 +0100

    découpage

--- rawudp.c
+++ rawudp.c
@@ -59,13 +59,13 @@ void readFileToSend(char *dt, char nameFile[]){
         while( div != 0){
             printf("\n %i",div);
 
-            char buffer = malloc(5);
+           /* char buffer = malloc(5);
             fseek(fichier, oct, SEEK_SET);
             fread(buffer, 5, 1, fichier);
             payload[div].packet = &buffer;           
             printf("%s\n", payload[div].packet);
 
-            //payload[div].packet = ;
+            //payload[div].packet = ;*/
             oct = oct + 5 ;
             div--;
         }
